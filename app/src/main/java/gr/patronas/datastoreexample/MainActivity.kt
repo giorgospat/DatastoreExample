3b5d23bf01e015ae030c0b3af7c5a165ee334c78
giorgos-patronas <giorgos.patronas@vodafone.com>
2021-02-25 12:32:12 +0200

    refactor exercise with datastore proto

--- app/src/main/java/gr/patronas/datastoreexample/MainActivity.kt
+++ app/src/main/java/gr/patronas/datastoreexample/MainActivity.kt
@@ -4,31 +4,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
-import gr.patronas.datastoreexample.datastore.UserPreferences
+import gr.patronas.datastoreexample.datastore.BookmarkDataStore
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var userPreferences: UserPreferences
+    private lateinit var bookmarkDataStore: BookmarkDataStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userPreferences = UserPreferences(this)
+        bookmarkDataStore = BookmarkDataStore(this)
 
         buttonSaveBookmark.setOnClickListener {
             val bookmark = editTextBookmark.text.toString().trim()
             lifecycleScope.launch {
-                userPreferences.saveBookmark(bookmark)
+                bookmarkDataStore.saveBookmark(bookmark)
             }
         }
 
-        userPreferences.bookmark.asLiveData().observe(this, {
+        bookmarkDataStore.bookmark.asLiveData().observe(this, {
             textViewCurrentBookmark.text = it
         })
 
     }
-    
+
 }
---
+++ app/src/main/java/gr/patronas/datastoreexample/datastore/BookmarkDataStore.kt
@@ -0,0 +1,58 @@
+package gr.patronas.datastoreexample.datastore
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.createDataStore
+import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import gr.patronas.datastoreexample.Bookmark
+import kotlinx.coroutines.flow.map
+import java.io.InputStream
+import java.io.OutputStream
+
+class BookmarkDataStore(
+    context: Context
+) {
+    private val applicationContext = context.applicationContext
+    private val dataStore: DataStore<Bookmark>
+
+    init {
+        dataStore = applicationContext.createDataStore(
+            fileName = "bookmark.pb",
+            serializer = BookmarkSerializer
+        )
+    }
+
+    val bookmark = dataStore.data
+        .map { bookmarkSchema ->
+            bookmarkSchema.bookmark
+        }
+
+    suspend fun saveBookmark(bookmark: String) {
+        dataStore.updateData { currentBookmark ->
+            currentBookmark.toBuilder()
+                .setBookmark(bookmark)
+                .build()
+        }
+    }
+
+    object BookmarkSerializer : Serializer<Bookmark> {
+        override fun readFrom(input: InputStream): Bookmark {
+            try {
+                return Bookmark.parseFrom(input)
+            } catch (exception: InvalidProtocolBufferException) {
+                throw CorruptionException("Cannot read proto.", exception)
+            }
+        }
+
+        override fun writeTo(
+            t: Bookmark,
+            output: OutputStream
+        ) = t.writeTo(output)
+
+        override val defaultValue: Bookmark
+            get() = Bookmark.getDefaultInstance()
+    }
+
+}
