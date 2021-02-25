4d38d9f896037794429ba3b2ffb4d3f5dcf0207b
giorgos-patronas <giorgos.patronas@vodafone.com>
2021-02-25 11:55:43 +0200

    update dependencies

--- app/src/main/java/gr/enginius/datastoreexample/datastore/UserPreferences.kt
+++ app/src/main/java/gr/enginius/datastoreexample/datastore/UserPreferences.kt
@@ -1,11 +1,11 @@
 package gr.enginius.datastoreexample.datastore
 
 import android.content.Context
-import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -13,13 +13,9 @@ class UserPreferences(
     context: Context
 ) {
     private val applicationContext = context.applicationContext
-    private val dataStore: DataStore<Preferences>
-
-    init {
-        dataStore = applicationContext.createDataStore(
-            name = "app_preferences"
-        )
-    }
+    private val dataStore: DataStore<Preferences> = applicationContext.createDataStore(
+        name = "app_preferences"
+    )
 
     val bookmark: Flow<String?>
         get() = dataStore.data.map { preferences ->
@@ -34,6 +30,6 @@ class UserPreferences(
 
 
     companion object {
-        val KEY_BOOKMARK = preferencesKey<String>("key_bookmark")
+        val KEY_BOOKMARK = stringPreferencesKey("key_bookmark")
     }
 }
